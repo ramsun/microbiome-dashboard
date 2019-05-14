47ea612709c5434a4d87a5e709c7a5a3fc9883d5
Ramamurthy Sundar <rsramamurthysundar@gmail.com>
2019-05-14 16:03:06 -0400

    Began traces

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
 function buildMetadata(sample) {
-
+  
   // @TODO: Complete the following function that builds the metadata panel
-
   // Use `d3.json` to fetch the metadata for a sample
+  var url = `/metadata/${sample}`;
     // Use d3 to select the panel with id of `#sample-metadata`
 
     // Use `.html("") to clear any existing metadata
@@ -18,7 +18,57 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url = `/samples/${sample}`;
+  d3.json(url).then(function(response) {
+  
+    var topSamples = response.sample_values.slice(0,10);
+    var topIds = response.otu_ids.slice(0,10);
+    //var sampleLabels = response.otu_labels;
+    console.log(topSamples);
+    //console.log(sampleIds);
+    
+    
+    var pieTrace = {
+      x: response.otu_ids,
+      y: response.sample_values,
+      type: 'pie'
+    };
 
+    var bubbleTrace = {
+      x: response.otu_ids,
+      y: [10, 11, 12, 13],
+      mode: 'markers',
+      marker: {
+        size: [40, 60, 80, 100]
+      }
+    };
+    
+    // var trace = {
+    //   type: "scatter",
+    //   mode: "lines",
+    //   name: "Bigfoot Sightings",
+    //   x: response.map(data => data.year),
+    //   y: response.map(data => data.sightings),
+    //   line: {
+    //     color: "#17BECF"
+    //   }
+    // };
+
+    // var data = [trace];
+
+    // var layout = {
+    //   title: "Bigfoot Sightings Per Year",
+    //   xaxis: {
+    //     type: "date"
+    //   },
+    //   yaxis: {
+    //     autorange: true,
+    //     type: "linear"
+    //   }
+    // };
+
+    // Plotly.newPlot("plot", data, layout);
+  });
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
